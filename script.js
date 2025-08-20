0618141c1570d1e42ef6dd4b95df045d8245f62d
Arthur Guimaraes <arthurguimaraes.contato@gmail.com>
2025-08-20 14:47:18 -0300

    Adicionando botões nas listas

---
+++ script.js
@@ -0,0 +1,10 @@
+const botaoExcluir = document.querySelectorAll(".button-delete");
+
+botaoExcluir.forEach(button => {
+    button.addEventListener('click', (e)=>{
+        const taskItem = e.target.closest('.task-item');
+        if (taskItem){
+            taskItem.remove();
+        }
+    })
+})
